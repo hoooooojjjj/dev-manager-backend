6d8a0630a238c15b1d8240d449a7fafc563cffe2
ryuhojun <rhj080471@gmail.com>
2025-10-07 23:21:12 +0900

    feat: mysql + type orm init

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "varchar", length: 255, unique: true })
+  email: string;
+
+  @Column({ type: "varchar", length: 100 })
+  name: string;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt: Date;
+}
